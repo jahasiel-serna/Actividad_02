fbe6cad71d6f1e6b3d47103dd9927e5b3a9640ac
Jahasiel Serna <jh.serna.b@alumnos.udg.com>
2020-09-21 20:22:38 -0500

    Solicita la edad para imprimir edad + 1

--- main.cpp
+++ main.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main() {
     string nombre;
+    int edad;
     cout << "Ingresa tu nombre: ";
     cin >> nombre;
-    cout << "Hola " << nombre << "!" << endl;
+    cout << "Ingresa tu edad: ";
+    cin >> edad;
+    cout << "Hola " << nombre << "!" << endl << "Vas a cumplir " << edad + 1 << endl;
     return 0;
 }
