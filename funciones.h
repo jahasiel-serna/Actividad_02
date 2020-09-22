0cee93ad479d68475d8755b1dd5432f628865129
Jahasiel Serna <jh.serna.b@alumnos.udg.com>
2020-09-22 15:32:22 -0500

    condiciones para suma y resta de numeros naturales

--- funciones.h
+++ funciones.h
@@ -2,11 +2,11 @@
 #define FUNCIONES.H
 
 int suma(int a, int b) {
-    return a + b;
+    return a < 0 ? a + b : 0;
 }
 
 int resta(int a, int b) {
-    return a + b;
+    return a < 0 ? a - b : 0;
 }
 
 #endif
